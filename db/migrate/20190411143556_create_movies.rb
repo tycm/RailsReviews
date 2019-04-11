b93bb0b5f3837e94ce1252d30b5db179552ee145
tmccrick <tmccrick@uncc.edu>
2019-04-11 15:07:52 +0000

    Migrated db

--- db/migrate/20190411143556_create_movies.rb
+++ db/migrate/20190411143556_create_movies.rb
@@ -1,9 +1,8 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
     create_table :movies do |t|
-      t.integer :id
       t.datetime :published_at
-      t.String :Title
+      t.string :title
 
       t.timestamps
     end
--- db/migrate/20190411150120_create_users.rb
+++ db/migrate/20190411150120_create_users.rb
@@ -1,7 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.integer :id
       t.string :name
 
       t.timestamps

9167d728e12f491382a93985f5630bf803360f46
tycm <tim.c@mccrickard.us>
2019-04-20 18:36:27 -0400

    Added CRUD functions to movies and reviews controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,42 @@
 class MoviesController < ApplicationController
+	def index
+        @movies = Movie.all
+    end
+    def show
+        @movie = Movie.find(params[:id])
+    end
+    def new
+        @movie = Movie.new
+    end
+    
+    def create
+        @movie = Movie.new(movie_params)
+        
+        if @movie.save
+            redirect_to @movie
+        else
+            render 'new'
+        end
+    end
+    def edit
+        @movie = Movie.find(params[:id])
+    end
+    def update
+        @movie = Movie.find(params[:id])
+        if @movie.update(movie_params)
+            redirect_to @movie
+        else
+            render 'edit'
+        end
+    end
+    def destroy
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+        redirect_to articles_path
+    end
 end
+
+private 
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,19 @@
 class ReviewsController < ApplicationController
+	def create
+        @movie = Movie.find(params[:movie_id])
+        @review = @Movie.reviews.create(review_params)
+        redirect_to article_path(@movie)
+    end
+    
+    def destroy
+        @movie = Movie.find(params[:movie_id])
+        @review = @movie.reviews.find(params[:id])
+        @review.destroy
+        redirect_to article_path(@Movie)
+    end
+    
+    private
+    def comment_params
+        params.require(:review).permit(:reviewer, :body)
+    end
 end

e8376d31915a06075319216802bd9e57d1e67e56
tycm <tim.c@mccrickard.us>
2019-04-20 19:17:25 -0400

    Updated some CSS

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ApplicationRecord
+	has_many :reviews, dependent: :destroy
+    validates :title, presence: true, length: { minimum: 5 }
+    validates :text, presence: true, length: { minimum: 10 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+	has_many :reviews, dependent: :destroy
+    validates :username, presence: true, length: { minimum: 5 }
 end

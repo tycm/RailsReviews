1276e9a1f9b31f9d334ce01aecb0de859a1e1e31
tycm <tim.c@mccrickard.us>
2019-04-28 01:37:09 -0400

    Cleaning up models and controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_04_21_022331) do
 
 end

3ab3086c7844fdadead71d1dffba5aa6f02c4367
tycm <tim.c@mccrickard.us>
2019-04-30 10:37:36 -0400

    Fixed indentation on destroy function

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -24,12 +24,12 @@ class MoviesController < ApplicationController
 	end
 
 
-def destroy
-	@movie = Movie.find(params[:id])
-	@movie.destroy
+	def destroy
+		@movie = Movie.find(params[:id])
+		@movie.destroy
 
-	redirect_to movies_path
-end
+		redirect_to movies_path
+	end
 
 	def create
 		@movie = Movie.new(movie_params)
